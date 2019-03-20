b1240ba240a561ae5d3ea091330ec12ff5f6f94a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-03-20 13:22:49 -0400

    removing unwanted spaces in the footer

--- partials/footer.blade.php
+++ partials/footer.blade.php
@@ -1,7 +1,3 @@
-▽
-<br/>
-<br/>
-<br/>
 <hr class="separation-line blue-bg"/>
 <div class="col-md-12 new-collaborators text-center">
     <div class="col-md-offset-2 col-md-2">
@@ -21,7 +17,7 @@
     </div>
         <div class="col-md-2">
                 <a href="pages/contact" target="_blank">
-                <img width="20%" src="{{URL::to('/')}}/themes/{{Session::get('theme')}}/assets/img/contact.png">
+                <img width="25%" src="{{URL::to('/')}}/themes/{{Session::get('theme')}}/assets/img/contact.png">
                 </a>
         </div>
 </div>
