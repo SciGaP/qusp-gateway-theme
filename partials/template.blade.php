12a55218bfd22968c4b6c895a0fe5d751bc70d38
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-03-28 10:05:13 -0400

    Adding some changes to mines logo size

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -14,7 +14,7 @@
 <div class="col-md-12"> <!-- breathing-space">-->
   <div class="row">
     <div class="col-md-offset-5 col-md-2">
-      <img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/mines-logo.png" width="150%"/>
+      <img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/mines-logo.png" width="100%"/>
       <hr/>
       <a href="{{ URL::to('/') }}/login">
         <h4>Login</h4>
@@ -27,8 +27,8 @@
     <div class="col-md-offset-3 col-md-8 scigap-info">
       <!-- <div class="col-md-offset-5 col-md-8 scigap-info">
       <p>
-      <a href="pages/about" class="button-sra" style="margin-right: 50px">About</a><a href="pages/documentation" class="button-sra" style="margin-right: 50px">Documentation</a>
-      <a href="pages/citeus" class="button-sra" style="margin-right: 50px">Cite Us</a><a href="pages/contact" class="button-sra">Contact</a>
+      <a href="pages/about" class="button-sra" style="margin-right: 30px">About</a><a href="pages/documentation" class="button-sra" style="margin-right: 30px">Documentation</a>
+      <a href="pages/citeus" class="button-sra" style="margin-right: 30px">Cite Us</a><a href="pages/contact" class="button-sra" style="margin-right: 30px">Contact</a>
       </p>
       -->
       </div>
