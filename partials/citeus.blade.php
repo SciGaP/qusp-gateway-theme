9af5f4cb8207fc6e32120e25f23c92331cc5cfc3
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-05-24 15:45:49 -0400

    Updated cite us page

--- partials/citeus.blade.php
+++ partials/citeus.blade.php
@@ -9,9 +9,14 @@
         -->
         <br></br>
         <h4 class="text-left">Acknowledgements</h4>
+
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        Please cite : "M. L. Wall and L. D. Carr, New J. Phys. 14, 125015 (2012)" and "D. Jaschke, M. L. Wall, and L. D. Carr, Computer Physics Communications 225, 59–91 (2018)" if your publication involves OSMPS.
+        </p>
         <p style="font-size:18px; margin-top:20px;" align="left">
         Development of the Apache Airavata used to develop the science gateway is supported by NSF award #1339774.
         </p>
+
     </div>
   </div>
 </div>
