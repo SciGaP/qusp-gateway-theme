4f58370ec4cc098c8b7d8e1a6ef51d41cefa333a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-05-24 15:48:23 -0400

    Updated Documentation us page

--- partials/footer.blade.php
+++ partials/footer.blade.php
@@ -3,12 +3,12 @@
 <div class="row new-collaborators text-center">
 <!--<div class="col-md-12 new-collaborators text-center">-->
     <div class="col-xs-offset-2 col-xs-2">
-        <a href="/pages/about" target="_blank">
+        <a href="https://people.mines.edu/lcarr/team/" target="_blank">
         <img width="40%" src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/people.png"/>
         </a>
     </div>
         <div class="col-xs-2">
-        <a href="/pages/documentation" target="_blank">
+        <a href="https://openmps.sourceforge.io" target="_blank">
         <img width="40%" src="{{URL::to('/')}}/themes/{{Session::get('theme')}}/assets/img/document.png">
         </a>
         </div>
